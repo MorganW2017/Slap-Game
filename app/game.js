c077a1f732f5274d90e58e0792d0254ee16efa5d
Morgan Waters <morangwaters@gmail.com>
2017-10-30 16:35:39 -0600

    falling change

--- app/game.js
+++ app/game.js
@@ -3,7 +3,7 @@ var reset = document.getElementById('reset')
 function update() {
 	if (health <= 0) {
 		health = 0;
-		document.getElementById('smoke').src = 'https://www194.lunapic.com/do-not-link-here-use-hosting-instead/150940193474175?620616655'
+		document.getElementById('smoke').src = '../assets/smoke-falling.gif'
 		reset.disabled = false
 	}
 	document.getElementById("health").innerText = health;
